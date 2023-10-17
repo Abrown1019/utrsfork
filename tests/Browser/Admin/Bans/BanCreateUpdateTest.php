f5481c3f231f3b1724c2eef4918cbf6a66de9f47
Abrown1019 <148242195+Abrown1019@users.noreply.github.com>
2023-10-17 13:56:14 -0500

    That's better

--- tests/Browser/Admin/Bans/BanCreateUpdateTest.php
+++ tests/Browser/Admin/Bans/BanCreateUpdateTest.php
@@ -12,14 +12,27 @@ class BanCreateUpdateTest extends DuskTestCase
     use DatabaseMigrations;
     use TestHasUsers;
 
-    public function test_non_tooladmin_cant_create_ban()
+    public function test_non_tooladmin_can_create_and_modify_ban()
     {
         $this->browse(function (Browser $browser) {
             $browser->visit('/changelang/en')
                 ->loginAs($this->getUser())
                 ->visit(route('admin.bans.create'))
-                ->assertSee('403')
-                ->assertDontSee('Add ban');
+                ->assertDontSee('403')
+                ->type('target', 'UTRS banned user')
+                ->type('reason', 'UTRS public ban reason')
+                ->type('comment', 'UTRS private ban comment')
+                ->press('Submit')
+                ->waitForText('Ban details',5)
+                ->waitForText('UTRS banned user',5)
+                ->waitForText('indefinite',5)
+                ->waitForText('English Wikipedia',5)
+                ->waitForText('Action: created, Reason: UTRS private ban comment',5)
+                ->type('reason', 'Another reason.')
+                ->click('[for=is_active-0]')
+                ->press('Save')
+                ->waitForText('unbanned',5);
+                
         });
     }
 
